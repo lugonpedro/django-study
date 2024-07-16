f61200128565bac2637da520b32ce23980a01ea5
Pedro Lugon <pedromotta20@gmail.com>
2024-07-16 00:15:16 -0300

    feat(): add update and delete with generic views

--- courses/urls.py
+++ courses/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import CoursesAPIView, RatingsAPIView
+from .views import CoursesAPIView, CourseAPIView, RatingsAPIView, RatingAPIView
 
 urlpatterns = [
     path('courses/', CoursesAPIView.as_view(), name='courses'),
-    path('ratings/', RatingsAPIView.as_view(), name='ratings')
+    path('courses/<int:pk>', CourseAPIView.as_view(), name='course'),
+    path('ratings/', RatingsAPIView.as_view(), name='ratings'),
+    path('ratings/<int:pk>', RatingAPIView.as_view(), name='rating')
 ]
--- courses/views.py
+++ courses/views.py
@@ -6,10 +6,14 @@ class CoursesAPIView(generics.ListCreateAPIView):
   queryset = Course.objects.all()
   serializer_class = CourseSerializer
 
-# class CourseAPIView(generics.RetrieveUpdateDestroyAPIView):
+class CourseAPIView(generics.RetrieveUpdateDestroyAPIView):
+  queryset = Course.objects.all()
+  serializer_class = CourseSerializer
 
 class RatingsAPIView(generics.ListCreateAPIView):
   queryset = Rating.objects.all()
   serializer_class = RatingSerializer
 
-# class RatingAPIView(generics.RetrieveUpdateDestroyAPIView):
+class RatingAPIView(generics.RetrieveUpdateDestroyAPIView):
+  queryset = Rating.objects.all()
+  serializer_class = RatingSerializer
