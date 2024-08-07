6d2d63caa37666d0a48349f34953d125a9d29173
Pedro Lugon <pedromotta20@gmail.com>
2024-08-07 18:08:31 -0300

    feat(): add viewsets and routers

--- courses/urls.py
+++ courses/urls.py
@@ -1,9 +1,16 @@
 from django.urls import path
-from .views import CoursesAPIView, CourseAPIView, RatingsAPIView, RatingAPIView
+from .views import CoursesAPIView, CourseAPIView, RatingsAPIView, RatingAPIView, RatingViewSet, CourseViewSet
+from rest_framework.routers import SimpleRouter
+
+router = SimpleRouter()
+router.register('courses', CourseViewSet)
+router.register('ratings', RatingViewSet)
 
 urlpatterns = [
     path('courses/', CoursesAPIView.as_view(), name='courses'),
     path('courses/<int:pk>', CourseAPIView.as_view(), name='course'),
+    path('courses/<int:pk>/ratings/', RatingsAPIView.as_view(), name='course_ratings'),
+    path('courses/<int:pk>/ratings/<int:rating_id>', RatingsAPIView.as_view(), name='course_rating'),
     path('ratings/', RatingsAPIView.as_view(), name='ratings'),
-    path('ratings/<int:pk>', RatingAPIView.as_view(), name='rating')
+    path('ratings/<int:rating_id>', RatingAPIView.as_view(), name='rating')
 ]
--- courses/views.py
+++ courses/views.py
@@ -1,7 +1,12 @@
+from django.shortcuts import get_object_or_404
 from rest_framework import generics
 from .models import Course, Rating
 from .serializers import CourseSerializer, RatingSerializer
+from rest_framework import viewsets, mixins
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
+# API V1
 class CoursesAPIView(generics.ListCreateAPIView):
   queryset = Course.objects.all()
   serializer_class = CourseSerializer
@@ -14,6 +19,38 @@ class RatingsAPIView(generics.ListCreateAPIView):
   queryset = Rating.objects.all()
   serializer_class = RatingSerializer
 
+  def get_queryset(self):
+    if self.kwargs.get('course_pk'):
+      return super().get_queryset().filter(course_id = self.kwargs.get('course_pk'))
+    return self.queryset.all()
+
 class RatingAPIView(generics.RetrieveUpdateDestroyAPIView):
+  queryset = Rating.objects.all()
+  serializer_class = RatingSerializer
+
+  def get_object(self):
+    if self.kwargs.get('course_pk'):
+      return get_object_or_404(
+        self.get_queryset(), course_id = self.kwargs.get('course_pk'), pk = self.kwargs.get('rating_pk'))
+    return super().get_object()
+
+# API V2
+class CourseViewSet(viewsets.ModelViewSet):
+  queryset = Course.objects.all()
+  serializer_class = CourseSerializer
+
+  @action(detail=True, methods=['get'])
+  def ratings(self, request, pj=None):
+    course = self.get_object()
+    serializer = RatingSerializer(course.ratings.all(), many=True)
+    return Response(serializer.data)
+  
+# VIEWSET PADRÃO
+# class RatingViewSet(viewsets.ModelViewSet):
+#   queryset = Rating.objects.all()
+#   serializer_class = RatingSerializer
+
+# VIEWSET CUSTOMIZADO
+class RatingViewSet(mixins.CreateModelMixin, mixins.RetrieveModelMixin, mixins.UpdateModelMixin, viewsets.GenericViewSet):
   queryset = Rating.objects.all()
   serializer_class = RatingSerializer
