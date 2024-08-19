9f6945bd4da40cffed803c7c4214cef6c006bbe0
Pedro Lugon <pedromotta20@gmail.com>
2024-08-19 16:45:31 -0300

    feat(): auth with token

--- school/settings.py
+++ school/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'courses',
-    'rest_framework'
+    'rest_framework',
+    'rest_framework.authtoken'
 ]
 
 MIDDLEWARE = [
@@ -127,7 +128,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # DRF
 REST_FRAMEWORK = {
   'DEFAULT_AUTHENTICATION_CLASSES': {
-    'rest_framework.authentication.SessionAuthentication'
+    # 'rest_framework.authentication.SessionAuthentication',
+    'rest_framework.authentication.TokenAuthentication'
   },
   'DEFAULT_PERMISSION_CLASSES': {
     'rest_framework.permissions.IsAuthenticatedOrReadOnly'
